fbe60b120c88f6d660ec17ab590c6c7a9b823a5f
Hansol Yun <hsyoon315@gmail.com>
2021-05-11 01:41:32 +0900

    ADD: Game component

---
+++ src/components/Game/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Table from "../Table";
+import Timer from "../Timer";
+import Order from "../Order";
+import Evaluation from "../Evaluation";
+import Revenue from "../Revenue";
+import { useSelector } from "react-redux";
+import TableTwo from "../TableTwo";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+function Game() {
+  const { sashimiOrder } = useSelector((state) => state.order);
+  return (
+    <Wrapper>
+      <Timer />
+      <Revenue />
+      <Order />
+      {sashimiOrder.name.length && <Evaluation />}
+      <Table />
+    </Wrapper>
+  );
+}
+
+export default Game;
