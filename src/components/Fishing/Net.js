93e5f2b4996971aa3a98e07054f0c07d5b57a674
Hansol Yun <hsyoon315@gmail.com>
2021-05-16 04:05:47 +0900

    ADD: changeFishCoordinate, drawFish in Net

--- src/components/Fishing/Net.js
+++ src/components/Fishing/Net.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -7,21 +8,55 @@ const Wrapper = styled.div`
   }
 `;
 
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 function Net() {
   const canvasRef = useRef(null);
+  const animationRef = useRef(null);
+  const fish = useSelector((state) => state.fishing.fish);
+
+  const changeFishCoordinate = () => {
+    for (let i = 0; i < fish.length; i++) {
+      fish[fish.length -1].x = getRandomInt(0, 200);
+      fish[fish.length -1].y = getRandomInt(0, 100);
+    }
+  }
+
+  const drawFish = (ctx) => {
+    for (let i = 0; i < fish.length; i++) {
+      fish[i].render(ctx);
+    }
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth * 0.2;
+    ctx.canvas.height = window.innerHeight * 0.2;
+
+    if (fish.length !== 0) {
+      changeFishCoordinate();
+    }
 
     const update = () => {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.fillStyle = "grey";
       ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      drawFish(ctx);
+      animationRef.current = requestAnimationFrame(update);
     };
 
     update();
-  }, []);
+
+    return () => {
+      cancelAnimationFrame(animationRef.current);
+    };
+  }, [fish.length]);
 
   return (
     <Wrapper>
