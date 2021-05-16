a6d27079b1842df2731e69e50758f4bf84caee44
Hansol Yun <hsyoon315@gmail.com>
2021-05-17 00:38:28 +0900

    ADD: modal in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import sushi from "./sushi";
 import order from "./order";
 import revenue from "./revenue";
 import fishing from "./fishing";
+import modal from "./modal";
 
 const reducers = combineReducers({
   sushi,
   order,
   revenue,
   fishing,
+  modal,
 });
 
 export default reducers;
