8cf9744387828c35d938dc273386410b055031d7
Hansol Yun <hsyoon315@gmail.com>
2021-05-18 03:30:40 +0900

    FIX: cdn font

--- src/styles/fonts/index.js
+++ src/styles/fonts/index.js
@@ -1,10 +1,9 @@
 import { createGlobalStyle } from "styled-components";
-import RixYeoljeongdo from "./RixYeoljeongdo_Regular.woff";
 
 const globalFonts = createGlobalStyle`
   @font-face {
-    font-family: "RixYeoljeongdo_Regular";
-    src: url(${RixYeoljeongdo}) format("woff");
+    font-family: 'RixYeoljeongdo_Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2102-01@1.0/RixYeoljeongdo_Regular.woff') format('woff');
     font-weight: normal;
     font-style: normal;
   }
