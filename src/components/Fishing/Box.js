8da76a1af026f24c08dfe464bf15d557176bfce1
Hansol Yun <hsyoon315@gmail.com>
2021-05-19 19:53:16 +0900

    ADD: hook image and refactor fish coordinate range

--- src/components/Fishing/Box.js
+++ src/components/Fishing/Box.js
@@ -2,12 +2,14 @@ import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import Fish from "./Fish";
+import Hook from "./Hook";
 import tuna from "../../asset/tuna_fishing.png";
 import salmon from "../../asset/salmon_fishing.png";
 import ocean from "../../asset/ocean.jpg";
 import eel from "../../asset/eel_fishing.png";
 import octopus from "../../asset/octopus_fishing.png";
 import shrimp from "../../asset/shrimp_fishing.png";
+import hookImage from "../../asset/hook.png";
 
 const Wrapper = styled.div`
   canvas {
@@ -28,17 +30,17 @@ let fishes;
 
 const createFish = (ctx) => {
   for (let i = 0; i < 6; i++) {
-    fishes.push(new Fish("tuna", getRandomInt(-10, 1000), getRandomInt(100, 450), 100, 30, tuna));
-    fishes.push(new Fish("salmon", getRandomInt(-10, 1000), getRandomInt(100, 450), 120, 30, salmon));
-    fishes.push(new Fish("eel", getRandomInt(-10, 1000), getRandomInt(300, 450), 90, 30, eel));
+    fishes.push(new Fish("tuna", getRandomInt(0, ctx.canvas.width / 2), getRandomInt(ctx.canvas.height * 0.2, ctx.canvas.height * 0.7), 100, 30, tuna));
+    fishes.push(new Fish("salmon", getRandomInt(ctx.canvas.width / 2, ctx.canvas.width), getRandomInt(ctx.canvas.height * 0.2, ctx.canvas.height * 0.7), 120, 30, salmon));
+    fishes.push(new Fish("eel", getRandomInt(0, ctx.canvas.width), getRandomInt(ctx.canvas.height * 0.4, ctx.canvas.height * 0.7), 90, 30, eel));
   }
 
   for (let i = 0; i < 4; i++) {
-    fishes.push(new Fish("octopus", getRandomInt(-10, 1000), 500, 80, 40, octopus));
+    fishes.push(new Fish("octopus", getRandomInt(-10, ctx.canvas.width), ctx.canvas.height * 0.9, 80, 40, octopus));
   }
 
   for (let i = 0; i < 10; i++) {
-    fishes.push(new Fish("shrimp", getRandomInt(-10, 1000), getRandomInt(100, 450), 50, 30, shrimp));
+    fishes.push(new Fish("shrimp", getRandomInt(-10, ctx.canvas.width), getRandomInt(ctx.canvas.height * 0.2, ctx.canvas.height * 0.7), 50, 30, shrimp));
   }
 
   for (let i = 0; i < fishes.length; i++) {
@@ -51,6 +53,7 @@ let isHookCreated = false;
 let intervalId;
 let caughtFish;
 let caughtFishIndex;
+let hook;
 
 const createFishingLine = (e) => {
   if (!isHookCreated) {
@@ -62,6 +65,8 @@ const createFishingLine = (e) => {
       color: "grey",
     }
   }
+
+  hook = new Hook(fishingLine.endX - 1, fishingLine.endY, 15, 20, hookImage);
 };
 
 const drawFishingLine = (ctx) => {
@@ -72,6 +77,8 @@ const drawFishingLine = (ctx) => {
   ctx.strokeStyle = fishingLine.color;
   ctx.stroke();
   ctx.beginPath();
+
+  hook.render(ctx);
 };
 
 const catchFish = (ctx) => {
@@ -101,7 +108,7 @@ function Box() {
   const modal = useSelector((state) => state.modal);
   fishes = [];
 
-  const decreaseHookPosition = (currentX, currentY, endX, endY, ctx) => {
+  const decreaseHookPosition = (currentX, currentY, endX) => {
     const xDecrement = 10;
     const yDecrement = 10;
 
@@ -125,6 +132,9 @@ function Box() {
         fishingLine.endX += xDecrement;
         fishingLine.endY -= yDecrement;
       }
+
+      hook.x = fishingLine.endX;
+      hook.y = fishingLine.endY - 10;
     }
   };
 
@@ -137,6 +147,7 @@ function Box() {
 
     fishingLine.endX = e.nativeEvent.offsetX;
     fishingLine.endY = e.nativeEvent.offsetY;
+
     intervalId = setInterval(() => {
       decreaseHookPosition(fishingLine.endX, fishingLine.endY, fishingLine.startX, fishingLine.startY);
     }, 100);
@@ -145,8 +156,10 @@ function Box() {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    ctx.canvas.width = window.innerWidth * 0.7;
-    ctx.canvas.height = window.innerHeight * 0.7;
+    // ctx.canvas.width = window.innerWidth * 0.7;
+    // ctx.canvas.height = window.innerHeight * 0.7;
+    ctx.canvas.width = document.body.clientWidth * 0.7;
+    ctx.canvas.height = document.body.clientHeight * 0.7;
 
     createFish(ctx);
 
@@ -176,6 +189,7 @@ function Box() {
         detectClick();
         if (caughtFish) {
           catchFish(ctx);
+          hook.render(ctx);
         }
       }
 
