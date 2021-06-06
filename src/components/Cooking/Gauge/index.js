c58dd7aa1e76359515c5ef9720ade73f0fb4e190
Hansol Yun <hsyoon315@gmail.com>
2021-06-06 23:31:03 +0900

    REFACTOR: seperate gauge container in Table

--- src/components/Cooking/Gauge/index.js
+++ src/components/Cooking/Gauge/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div``;
+import styled, { keyframes } from "styled-components";
+import wasabiImage from "../../../assets/image/wasabi.png";
 
 const Bar = styled.div`
   width: 400px;
@@ -15,13 +14,109 @@ const Percentage = styled.h1`
   font-family: RixYeoljeongdo_Regular;
 `;
 
-function Gauge({ percentage }) {
+const GaugeContainer = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+
+  *, *::after, *::before {
+    -webkit-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-app-region: no-drag;
+  }
+`;
+
+const WasabiContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30%;
+  height: auto;
+`;
+
+const Wasabi = styled.img`
+width: 80%;
+  padding: 8px;
+  cursor: pointer;
+`;
+
+const pulse = keyframes`
+  from {
+    transform: scale(1.5);
+  }
+  to {
+    transform: scale(1);
+  }
+`;
+
+const Guide = styled.div`
+  width: 50%;
+  height: 100%;
+
+  *, *::after, *::before {
+    -webkit-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-app-region: no-drag;
+    cursor: default;
+  }
+`;
+
+const Text = styled.p`
+  font-family: "RixYeoljeongdo_Regular";
+  font-size: 30px;
+  padding: 10px;
+
+  &.pulse {
+    animation: ${pulse} 0.5s ease-out infinite;
+  }
+`;
+
+const Item = styled.div`
+  width: 120px;
+  cursor: grab;
+  background-color: white;
+  border-radius: 10px;
+  margin: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+`;
+
+function Gauge({
+  percentage,
+  updatePercentage,
+  wasabiOrder,
+  rice
+}) {
   return (
-    <Wrapper>
-      <Percentage>{`${percentage}%`}</Percentage>
+    <GaugeContainer>
+      <Percentage>
+        {`${percentage}%`}
+      </Percentage>
       <Bar percentage={percentage} />
-    </Wrapper>
+      <WasabiContainer>
+        <Item>
+          <Wasabi
+            className="wasabi"
+            src={wasabiImage}
+            alt="wasabi"
+            onClick={updatePercentage}
+            draggable="false"
+          />
+        </Item>
+        {rice.id && wasabiOrder !== 0
+        && (
+          <Guide>
+            <Text className="pulse">
+              ←클릭!
+            </Text>
+          </Guide>
+        )}
+      </WasabiContainer>
+    </GaugeContainer>
   );
 }
 
-export default Gauge;
+export default React.memo(Gauge);
