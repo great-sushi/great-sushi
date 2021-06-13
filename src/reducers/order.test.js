b86bee02b287c0a403017703038f3eae44e7ed92
Hansol Yun <hsyoon315@gmail.com>
2021-06-13 22:50:32 +0900

    TEST: add order reducer test

---
+++ src/reducers/order.test.js
@@ -0,0 +1,37 @@
+import order from "./order";
+import { ADD_ORDER } from "../constants";
+
+describe("reducers", () => {
+  describe("order", () => {
+    const initialState = {
+      sashimiOrder: {
+        id: "",
+        name: "",
+      },
+      wasabiOrder: 0,
+    };
+
+    it("should provide initial state", () => {
+      expect(order(undefined, {})).toEqual(initialState);
+    });
+
+    it("should handle ADD_ORDER action", () => {
+      expect(
+        order(initialState, {
+          type: ADD_ORDER,
+          sashimi: {
+            id: "egg",
+            name: "계란",
+          },
+          wasabi: 50,
+        })
+      ).toEqual({
+        sashimiOrder: {
+          id: "egg",
+          name: "계란",
+        },
+        wasabiOrder: 50,
+      });
+    });
+  });
+});
